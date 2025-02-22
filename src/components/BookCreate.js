b67be430c31d1e68e73b753312e6c0f6ca8a0f09
Nicolas <mohamedsabirr2000@gmail.com>
2025-02-22 21:14:17 +0300

    The basic parts are finishid

--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -1,4 +1,25 @@
-function BookCreate() {
-  return <div>BookCreate</div>;
+import { useState } from "react";
+function BookCreate({ onCreate }) {
+  const [title, setTitle] = useState("");
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onCreate(title);
+    setTitle("");
+  };
+  return (
+    <div className="book-create">
+      <h3>Add a book</h3>
+      <form onSubmit={handleSubmit}>
+        <label>title</label>
+        <input className="input" value={title} onChange={handleChange} />
+        <button className="button">Create</button>
+      </form>
+    </div>
+  );
 }
 export default BookCreate;
