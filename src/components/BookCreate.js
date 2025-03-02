1cb1c54d40414873edc48014171b54003561f941
Nicolas <mohamedsabirr2000@gmail.com>
2025-03-02 16:04:38 +0300

    The code has been refactor to use context

--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -1,6 +1,8 @@
-import { useState } from "react";
-function BookCreate({ onCreate }) {
+import { useState, useContext } from "react";
+import BooksContext from "../context/books";
+function BookCreate() {
   const [title, setTitle] = useState("");
+  const { createBook } = useContext(BooksContext);
 
   const handleChange = (event) => {
     setTitle(event.target.value);
@@ -8,7 +10,7 @@ function BookCreate({ onCreate }) {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    onCreate(title);
+    createBook(title);
     setTitle("");
   };
   return (
