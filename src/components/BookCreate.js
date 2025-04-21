22e4206b0d61f5b66e0f97229ad779d4e84d5c47
mo-sa-mo <mohamedsabirr1999@gmail.com>
2025-04-21 17:09:04 +0300

    code has been refactored

--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -1,6 +1,9 @@
-import { useState } from "react";
-function BookCreate({ onCreate }) {
+import { useState, useContext } from "react";
+import BooksContext from "../context/books";
+
+function BookCreate() {
   const [title, setTitle] = useState("");
+  const { createBook } = useContext(BooksContext);
 
   const handleChange = (event) => {
     setTitle(event.target.value);
@@ -8,18 +11,20 @@ function BookCreate({ onCreate }) {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    onCreate(title);
+    createBook(title);
     setTitle("");
   };
+
   return (
     <div className="book-create">
-      <h3>Add a book</h3>
+      <h3>Add a Book</h3>
       <form onSubmit={handleSubmit}>
-        <label>title</label>
+        <label>Title</label>
         <input className="input" value={title} onChange={handleChange} />
-        <button className="button">Create</button>
+        <button className="button">Create!</button>
       </form>
     </div>
   );
 }
+
 export default BookCreate;
--- src/components/BookEdit.js
+++ src/components/BookEdit.js
@@ -1,6 +1,9 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import BooksContext from "../context/books";
+
 function BookEdit({ book, onSubmit }) {
   const [title, setTitle] = useState(book.title);
+  const { editBookById } = useContext(BooksContext);
 
   const handleChange = (event) => {
     setTitle(event.target.value);
@@ -8,8 +11,11 @@ function BookEdit({ book, onSubmit }) {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    onSubmit(book.id, title);
+
+    onSubmit();
+    editBookById(book.id, title);
   };
+
   return (
     <form onSubmit={handleSubmit} className="book-edit">
       <label>Title</label>
@@ -18,4 +24,5 @@ function BookEdit({ book, onSubmit }) {
     </form>
   );
 }
+
 export default BookEdit;
